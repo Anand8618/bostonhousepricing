4e8125f2e29092afd9ad2f10758660e88a0cfdab
Anand8618 <rockingandy229@gmail.com>
2022-09-01 00:45:22 +0530

    Web app ready

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# from crypt import methods
 import pickle
+from typing import final
 from flask import Flask, render_template,request,app,jsonify,url_for
 import numpy as np
 import pandas as pd
@@ -14,7 +16,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data=request.json['data']
     print(data)
@@ -23,6 +24,14 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
